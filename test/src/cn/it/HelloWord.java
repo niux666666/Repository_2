cf1b28dba0e9d019c522c904e2cd16f812ec126a
nx <aaa@qq.com>
2019-06-09 20:57:10 +0800

    Merge branch 'master' of F:\old\Git_test with conflicts.

--- test/src/cn/it/HelloWord.java
+++ test/src/cn/it/HelloWord.java
@@ -3,6 +3,6 @@ package cn.it;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
-        System.out.println("HelloWord");
+        System.out.println("aaa");
     }
 }
