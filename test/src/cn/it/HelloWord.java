7d5403a01ac70eda391d2402a1fd1d9f8ffd76fb
nx <aaa@qq.com>
2019-06-10 09:20:21 +0800

    修改

--- test/src/cn/it/HelloWord.java
+++ test/src/cn/it/HelloWord.java
@@ -3,12 +3,5 @@ package cn.it;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
-        System.out.println("aaa");
-        System.out.println("bbb");
-        System.out.println("ccc");
-        System.out.println("fff");
-        System.out.println("fff");
-        System.out.println("ggg");
-
     }
 }
