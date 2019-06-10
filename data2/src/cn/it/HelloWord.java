ca2fa46ae98f0d6114bbeaf897d8177d6794a6cf
nx <aaa@qq.com>
2019-06-10 10:48:10 +0800

    a

--- data2/src/cn/it/HelloWord.java
+++ data2/src/cn/it/HelloWord.java
@@ -3,11 +3,5 @@ package cn.it;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("HelloWord");
-		System.out.println("aaa");
-        System.out.println("bbb");
-		System.out.println("ccc");
-		System.out.println("ddd");
-        System.out.println("eee");
-		System.out.println("fff");
     }
 }
